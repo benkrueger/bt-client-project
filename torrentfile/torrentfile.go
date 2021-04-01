0e04ebb83617bde87267cbf485fe651eb5935a93
Benjamin Krueger <bokkw4@umsystem.edu>
2021-03-31 20:11:17 -0500

    added bencode file parsing code, its really rough right now but it should work

--- torrentfile/torrentfile.go
+++ torrentfile/torrentfile.go
@@ -7,5 +7,126 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"regexp"
 	bencode "github.com/IncSW/go-bencode"
 )
+
+var ANNOUCEREG = regexp.MustCompile("(udp|https?)://([^/^:]+)")
+
+type TorrentInfo struct {
+	filepath string 
+	announce string 
+	scrape string 
+	tracker string 
+	name string 
+	length string 
+	contents []string 
+	infohash string
+	private bool
+}
+
+type Torrent struct {
+	pieces string
+}
+func (t *Torrent) UnmarshalTfBytes(b []byte) {
+	data, err := bencode.Unmarshal(b)
+	if err == nil {
+
+		buf_m_s_i := data.(map[string]interface{})
+		announce_url_str := string(buf_m_s_i["announce"].([]byte))
+		info_dict := buf_m_s_i["info"].(map[string]interface{})
+		info_bytes, _ := bencode.Marshal(info_dict)
+
+		t.infohash = fmt.Sprintf("%x", sha1.Sum([]byte(info_bytes)))
+		t.name = string(info_dict["name"].([]byte))
+
+		if info_dict["private"] != nil {
+			if info_dict["private"].(int64) == 1 {
+				t.private = true
+			}
+		} else {
+			t.private = false
+		}
+		files_list := info_dict["files"]
+		t.announce = announce_url_str
+		t.SetTracker()
+
+		if files_list != nil {
+
+			contained_files_list := files_list.([]interface{})
+			for _, f := range contained_files_list {
+
+				f_dict := f.(map[string]interface{})
+				f_paths := f_dict["path"].([]interface{})
+				f_size := f_dict["length"].(int64)
+				t.length += f_size
+				for _, p := range f_paths {
+					path_string := string(p.([]byte))
+					t.contents = append(t.contents, path_string)
+				}
+			}
+
+		} else {
+			t.length = info_dict["length"].(int64)
+		}
+	}
+}
+func (ti *TorrentInfo)OpenTorrentInfo(file_path string) {
+
+
+}
+func (t *TorrentInfo)OpenFile(file_path string) {
+
+}
+func (t *TorrentInfo) GetTracker() string {
+	return t.tracker
+}
+func (t *TorrentInfo) GetFilePath() string {
+	return t.filepath
+}
+func (t *TorrentInfo) GetLength() int64 {
+	return t.length
+}
+func (t *TorrentInfo) GetName() string {
+	return t.name
+}
+func (t *TorrentInfo) GetPrivate() bool {
+	return t.private
+}
+func (t *TorrentInfo) GetInfohash() string {
+	return t.infohash
+}
+func (t *TorrentInfo) GetAnnouce() string {
+	return t.announce
+}
+
+func (t *TorrentInfo) SetTracker() {
+	if t.announce != "" {
+		t.tracker = ANNOUCEREG.FindStringSubmatch(t.announce)[2]
+		t.scrape = strings.Replace(t.announce, "announce", "scrape", -1)
+	}
+}
+
+
+func OpenTfile(path string) ([]byte, bool) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, false
+	}
+	defer f.Close()
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, false
+	}
+	return b, true
+}
+func (t *Torrent) PrintTorrentInfo() {
+	fmt.Println("File: ", t.filepath)
+	fmt.Println("Name :", t.name)
+	fmt.Println("Size :", t.length)
+	fmt.Println("Tracker :", t.tracker)
+	fmt.Println("Infohash :", string(t.infohash))
+	fmt.Println("Announce :", t.announce)
+	fmt.Println("Scrape :", t.scrape)
+	fmt.Println("Private :", t.private)
+}
